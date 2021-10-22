a852a3cb29728a194243833d45db5ba1efc534bf
Ajitomi, Daisuke <ajitomi@gmail.com>
2021-10-23 07:56:44 +0900

    Update package metadata.

--- flask_paseto_extended/__init__.py
+++ flask_paseto_extended/__init__.py
@@ -4,13 +4,13 @@ from .login_manager import PasetoLoginManager
 __version__ = "0.0.1"
 __title__ = "Flask PASETO Extended"
 __description__ = "PASETO (Platform-Agnostic Security Tokens) for Flask applications."
-__url__ = "https://github.com/dajiaji/flask-paseto"
+__url__ = "https://github.com/dajiaji/flask-paseto-extended"
 __uri__ = __url__
 __doc__ = __description__ + " <" + __uri__ + ">"
-__author__ = "AJITOMI Daisuke"
-__email__ = "ajitomi@gmail.com"
+__author__ = "Ajitomi Daisuke"
+__email__ = "dajiaji@gmail.com"
 __license__ = "MIT"
-__copyright__ = "Copyright 2021 AJITOMI Daisuke"
+__copyright__ = "Copyright 2021 Ajitomi Daisuke"
 __all__ = [
     "PasetoCookieSessionInterface",
     "PasetoLoginManager",
