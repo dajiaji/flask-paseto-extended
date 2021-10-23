2688119c2dbe0ec640067b05c7347208d5debd04
Ajitomi, Daisuke <ajitomi@gmail.com>
2021-10-23 11:31:23 +0900

    Bump up version to v0.1.1.

--- flask_paseto_extended/__init__.py
+++ flask_paseto_extended/__init__.py
@@ -1,7 +1,7 @@
 from .cookie_session import PasetoCookieSessionInterface
 from .login_manager import PasetoLoginManager
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __title__ = "Flask PASETO Extended"
 __description__ = "PASETO (Platform-Agnostic Security Tokens) for Flask applications."
 __url__ = "https://github.com/dajiaji/flask-paseto-extended"
